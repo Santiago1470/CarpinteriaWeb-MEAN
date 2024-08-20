84951b4222eb5c8fc3c1934c239088b200873679
Santiago1470 <st147086@gmail.com>
2024-08-20 00:18:28 -0500

    Cambio en diseño adaptativo

--- web-Carpinteria/src/app/componentes/catalogo/catalogo.component.ts
+++ web-Carpinteria/src/app/componentes/catalogo/catalogo.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-catalogo',
-  standalone: true,
-  imports: [],
-  templateUrl: './catalogo.component.html',
-  styleUrl: './catalogo.component.css'
+    selector: 'app-catalogo',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './catalogo.component.html',
+    styleUrl: './catalogo.component.css'
 })
+
 export class CatalogoComponent {
+    isOrden: boolean = false;
+    isFiltros: boolean = false;
+    
+    mostrarOrden() {
+        if(this.isOrden) {
+            this.isOrden = false;
+        } else {
+            this.isOrden = true;
+        }
+    }
+
+    ocultarOrden() {
+        this.isOrden = false;
+    }
+
+    mostrarFiltros(){
+        this.isFiltros = true;
+    }
+
+    ocultarFiltro() {
+        this.isFiltros = false;
+    }
 
 }
